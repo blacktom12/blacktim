1f9144f3630e4a1dd81df7211244be8bd10c4c55
blacktom12 <119161028+blacktom12@users.noreply.github.com>
2022-11-27 15:12:11 +0500

    Hello world

---
+++ HelloWorld.cs
@@ -0,0 +1,107 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour 
+{
+    // Start is called before the first frame updateññ
+    void Start()
+    {
+        //Âûâîä
+        print("Hello,World");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
